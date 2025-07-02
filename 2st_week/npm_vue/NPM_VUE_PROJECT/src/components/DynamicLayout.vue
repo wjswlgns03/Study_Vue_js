5094c0656a53f14e6870f9942fbdda6ff771aebd
wjswlgns03 <jjh030726@tukorea.ac.kr>
2025-07-02 23:07:39 +0900

    Vue.js 컴포넌트 상호작용 및 슬롯 예시 실습

---
+++ 2st_week/npm_vue/NPM_VUE_PROJECT/src/components/DynamicLayout.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <div>
+    <slot name="header"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ 2st_week/npm_vue/NPM_VUE_PROJECT/src/components/ParentChild.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>ParentChild 컴포넌트</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('부모 숫자:', this.$parent.parentNum);
+    console.log('부모 숫자 홀/짝:', this.$parent.numOddEven);
+    console.log('부모 숫자 가져오기:', this.$parent.getParentNum());
+  }
+};
+</script>
